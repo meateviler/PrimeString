162ed1cdb4fc30356ca493d305960292035fb231
meateviler <meateviler@msn.com>
2017-07-21 12:44:09 +0800

    codewar PrimeString

---
+++ PrimeString/Kata.cs
@@ -0,0 +1,19 @@
+﻿namespace PrimeString
+{
+	public class Kata
+	{
+		public bool PrimeString(string s)
+		{
+			if (s.Length <= 1)
+				return true;
+			for (int i = 1; i <= s.Length/2; i++)
+			{
+				var patern = s.Substring(0, i);
+				var tmp = s;
+				if (tmp.Replace(patern, string.Empty) == string.Empty)
+					return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ PrimeStringTests/KataTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace PrimeString.Tests
+{
+	[TestFixture()]
+	public class KataTests
+	{
+		[TestCase("x", ExpectedResult = true, TestName = "Input_x_shouldBe_true")]
+		[TestCase("abac", ExpectedResult = true, TestName = "Input_abac_shouldBe_true")]
+		[TestCase("abab", ExpectedResult = false, TestName = "Input_abab_shouldBe_false")]
+		[TestCase("aaaa", ExpectedResult = false, TestName = "Input_aaaa_shouldBe_false")]
+		[TestCase("abc", ExpectedResult = true, TestName = "Input_abc_shouldBe_true")]
+		[TestCase("fdsyffdsyffdsyffdsyffdsyf", ExpectedResult = false, TestName = "Input_fdsyffdsyffdsyffdsyffdsyf_shouldBe_false")]
+		[TestCase("utdutdtdutd", ExpectedResult = true, TestName = "Input_utdutdtdutd_shouldBe_true")]
+		[TestCase("abba", ExpectedResult = true, TestName = "Input_abba_shouldBe_true")]
+		public bool PrimeStringTest(string input)
+		{
+			var target = new Kata();
+			return target.PrimeString(input);
+		}
+	}
+}
